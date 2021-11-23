570f13c39aa7f4bdf97296bf1b72ab4a43501d80
Max <serfis.max@gmail.com>
2021-11-23 05:19:52 +0300

    Add files via upload

---
+++ Task1_1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Hello World!!!");
+        ReadKey();
+    }
+}
---
+++ Task1_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        Write("Hello ");
+        Write("World");
+        Write("!!!");
+        ReadLine();
+    }
+}
